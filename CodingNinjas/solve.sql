5e73c0657737b0c934a6554414f0c151bf4626eb
huylt@rikkeisoft.com <huylt@rikkeisoft.com>
2022-11-09 18:33:34 +0700

    z

--- CodingNinjas/solve.sql
+++ CodingNinjas/solve.sql
@@ -40,4 +40,35 @@ where domestic > 100000000
   and IMDB.title like '%2012%';
 
 /*https://www.codingninjas.com/codestudio/problems/imdb_1755912?topList=top-100-sql-problems*/
-select genre, max()
+select genre, max(net_profit) as net_profit
+from (select genre, sum(Worldwide + Domestic - Budget) as net_profit
+      from (select Title, genre, Domestic, Worldwide, Budget
+            from genre g
+                     inner join earning e on g.Movie_id = e.Movie_id
+                     inner join (select * from IMDB where Title like '%(2012)') as i on i.Movie_id = g.Movie_id) as info
+      group by Title, genre) as NP
+where genre is not null
+  and net_profit is not null
+group by genre
+order by genre;
+
+/*https://www.codingninjas.com/codestudio/problems/imdb_1755913?topList=top-100-sql-problems*/
+select genre, max(Rating) as weighted_rating
+from (select genre, (Rating + MetaCritic / 10) / 2 as Rating
+      from IMDB
+               inner join genre g on IMDB.Movie_id = g.Movie_id
+      where Title like '%(2014)'
+        and genre is not null
+        and Rating is not null
+        and MetaCritic is not null) as movie
+group by genre
+order by genre;
+#
+select genre, max((Rating + MetaCritic / 10) / 2) as weighted_rating
+from genre
+         natural join IMDB
+where IMDB.Title like '%2014%'
+  and genre.genre is not null
+group by genre
+order by genre;
+
